79497c99ecbfdf0b6db08fbbc56c9ccdccfef201
Tiago Gomes <tidgomes@gmail.com>
2025-03-05 13:10:44 -0300

    chore: add jest plugin in eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import jest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  jest,
   ...tseslint.configs.recommended,
 ];
