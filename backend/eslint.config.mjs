4735dce3731f7fe5dba86ff8928d972cec6cb740
Tiago Gomes <tidgomes@gmail.com>
2025-03-07 09:24:21 -0300

    chore: properly config the eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,9 +5,23 @@ import jest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["node_modules/", ".git/", "dist/", "coverage/"],
+  },
+  {
+    languageOptions: { globals: globals.node },
+  },
   pluginJs.configs.recommended,
-  jest,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      jest,
+    },
+    rules: {
+      ...jest.configs.recommended.rules,
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+    },
+  },
 ];
